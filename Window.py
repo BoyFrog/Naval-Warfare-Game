72c27bf48140a89af67350872ead856efc983542
BoyFrog <boyfrog2002@icloud.com>
2020-10-14 10:06:05 +1300

    Changing controls of ship - Making it acceleration based

--- Window.py
+++ Window.py
@@ -11,9 +11,38 @@ import arcade, random
 from PointClass import Point
 
 SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 800
+SCREEN_HEIGHT = 650
 SCREEN_TITLE = "Naval Warfare Game"
-SCALING = 1
+SCALING = 0.75
+
+# Speed limit
+MAX_SPEED = 3.0
+
+# How fast we accelerate
+ACCELERATION_RATE = 0.1
+
+
+class Player(arcade.Sprite):
+
+    def update(self):
+        self.center_x += self.change_x
+        self.center_y += self.change_y
+
+        # Check to see if we hit the screen edge
+        if self.left < 0:
+            self.left = 0
+            self.change_x = 0 # Zero x speed
+        elif self.right > SCREEN_WIDTH - 1:
+            self.right = SCREEN_WIDTH - 1
+            self.change_x = 0
+
+        if self.bottom < 0:
+            self.bottom = 0
+            self.change_y = 0
+        elif self.top > SCREEN_HEIGHT - 1:
+            self.top = SCREEN_HEIGHT - 1
+            self.change_y = 0
+
 
 class MyGame(arcade.Window):
     """
@@ -32,22 +61,42 @@ class MyGame(arcade.Window):
         self.ship_list = None
         self.all_sprites = None
 
+        # Variables that will hold sprite lists
+        self.player_list = None
+
+        # Set up the player info
+        self.player_sprite = None
+
+        # Track the current state of what key is pressed
+        self.left_pressed = False
+        self.right_pressed = False
+        self.up_pressed = False
+        self.down_pressed = False
+
         arcade.set_background_color(arcade.color.OCEAN_BOAT_BLUE)
 
     def setup(self):
         """ Set up the game variables. Call to re-start the game. """
         # Create your sprites and sprite lists here
+        """
         self.ship_list = arcade.SpriteList()
         self.all_sprites = arcade.SpriteList()
 
-        self.player_sprite = arcade.Sprite("Images/Ship1-x2.png")
+        self.player_sprite = arcade.Sprite("Images/Ship1-x2.png", SCALING)
         self.player_sprite.center_y = random.randrange(SCREEN_HEIGHT)
         self.player_sprite.center_x = random.randrange(SCREEN_WIDTH)
 
-
         self.ship_list.append(self.player_sprite)
         self.all_sprites.append(self.player_sprite)
+        """
+        # Sprite lists
+        self.player_list = arcade.SpriteList()
 
+        # Set up the player
+        self.player_sprite = Player("Images/Ship1-x2.png", SCALING)
+        self.player_sprite.center_x = SCREEN_WIDTH/2
+        self.player_sprite.center_y = SCREEN_HEIGHT/2
+        self.player_list.append(self.player_sprite)
 
     def on_draw(self):
         """
@@ -57,9 +106,10 @@ class MyGame(arcade.Window):
         # This command should happen before we start drawing. It will clear
         # the screen to the background color, and erase what we drew last frame.
         arcade.start_render()
-        self.all_sprites.draw()
 
         # Call draw() on all your sprite lists below
+        # Draw all the sprites.
+        self.player_list.draw()
 
     def on_update(self, delta_time):
         """
@@ -67,7 +117,29 @@ class MyGame(arcade.Window):
         Normally, you'll call update() on the sprite lists that
         need it.
         """
-        pass
+        # Apply acceleration based on the keys pressed
+        if self.up_pressed and not self.down_pressed:
+            self.player_sprite.change_y += ACCELERATION_RATE
+        elif self.down_pressed and not self.up_pressed:
+            self.player_sprite.change_y += -ACCELERATION_RATE
+        if self.left_pressed and not self.right_pressed:
+            self.player_sprite.change_x += -ACCELERATION_RATE
+        elif self.right_pressed and not self.left_pressed:
+            self.player_sprite.change_x += ACCELERATION_RATE
+
+        if self.player_sprite.change_x > MAX_SPEED:
+            self.player_sprite.change_x = MAX_SPEED
+        elif self.player_sprite.change_x < -MAX_SPEED:
+            self.player_sprite.change_x = -MAX_SPEED
+        if self.player_sprite.change_y > MAX_SPEED:
+            self.player_sprite.change_y = MAX_SPEED
+        elif self.player_sprite.change_y < -MAX_SPEED:
+            self.player_sprite.change_y = -MAX_SPEED
+
+        # Call update to move the sprite
+        # If using a physics engine, call update on it instead of the sprite
+        # list.
+        self.player_list.update()
 
     def on_key_press(self, key, key_modifiers):
         """
@@ -76,34 +148,27 @@ class MyGame(arcade.Window):
         For a full list of keys, see:
         http://arcade.academy/arcade.key.html
         """
-        pass
+        if key == arcade.key.UP:
+            self.up_pressed = True
+        elif key == arcade.key.DOWN:
+            self.down_pressed = True
+        elif key == arcade.key.LEFT:
+            self.left_pressed = True
+        elif key == arcade.key.RIGHT:
+            self.right_pressed = True
 
     def on_key_release(self, key, key_modifiers):
         """
         Called whenever the user lets off a previously pressed key.
         """
-        pass
-
-    def on_mouse_motion(self, x, y, delta_x, delta_y):
-        """
-        Called whenever the mouse moves.
-        """
-        self.player_sprite.center_x = x
-        self.player_sprite.center_y = y
-
-
-    def on_mouse_press(self, x, y, button, key_modifiers):
-        """
-        Called when the user presses a mouse button.
-        """
-        pass
-
-    def on_mouse_release(self, x, y, button, key_modifiers):
-        """
-        Called when a user releases a mouse button.
-        """
-        pass
-
+        if key == arcade.key.UP:
+            self.up_pressed = False
+        elif key == arcade.key.DOWN:
+            self.down_pressed = False
+        elif key == arcade.key.LEFT:
+            self.left_pressed = False
+        elif key == arcade.key.RIGHT:
+            self.right_pressed = False
 
 def main():
     """ Main method """
